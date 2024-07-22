65a5566a59608f75dca56899285a7b840cc2672d
jheremy val <2113110046@untels.edu.pe>
2024-07-22 15:49:30 -0500

    Valdivieso Examen Final - Parte 2 - Base de datos

--- ExamenFinalLp3/miapp/views.py
+++ ExamenFinalLp3/miapp/views.py
@@ -1,35 +1,35 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
+import sqlite3
 
 # Create your views here.
-
 
 layout = """
     <h2> Examen Final (LP3 - 2024) | Valdivieso Sánchez, Jheremy </h2>
     <hr/>
-    
 """
 
-
-
 def index(request):
-    mensaje="""
-    <ul>
-        <li>
-            <a href="/personas"> Opción Personas</a>
-        </li>
-    </ul>    
-    
-    <hr/>
-    
-        (Página de inicio)
-    """
-    return HttpResponse(layout+mensaje)
-
-
+    return render(request, 'index.html')
 
 def personas(request):
-    mensaje ="""
-        <h1>Pagina de personas</h1>
-    """
-    return HttpResponse(layout+mensaje)
-
+    conexion = sqlite3.connect("Valdiviesopersonas.db")
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM Valdivieso_persona")
+    personas = []
+    
+    for row in cursor.fetchall():
+        if len(row) >= 3:
+            fecha_registro = ""
+        else:
+            fecha_registro = None
+        personas.append({
+            'id': row[0],
+            'nombre': row[1],
+            'apellidos': row[2],
+            'sexo': row[3],
+            'fecha_de_registro': fecha_registro
+        })
+    cursor.close()
+    conexion.close()
+   
+    return render(request, 'personas.html', {'personas': personas})
